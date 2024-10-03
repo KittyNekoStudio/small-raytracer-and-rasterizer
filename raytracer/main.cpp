df6a8154662572fa91f8937ec17903178f278a6d
cat <kittynekostudio@gmail.com>
2024-10-02 17:50:29 -0700

    Finished the light section of the book.

--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -1,6 +1,5 @@
 #include "raylib.h"
 #include "vec3.h"
-
 #include <array>
 #include <cmath>
 #include <limits>
@@ -19,25 +18,69 @@ class Sphere {
     vec3 center;
     double radius;
     Color color;
+    int specular;
 
     Sphere() {}
 
-    Sphere(vec3 cen, double rad, Color col) {
+    Sphere(vec3 cen, double rad, Color col, int spec) {
         center = cen;
         radius = rad;
         color = col;
+        specular = spec;
+    }
+};
+
+enum LightType { ambient, point, directional };
+
+class Light {
+  public:
+    // TODO! find a way to separate the light type
+    // so that I don't need the direction for the ambient light
+    LightType type;
+    float intensity;
+    vec3 direction;
+    vec3 position;
+
+    Light() {}
+
+    Light(LightType light, float intens, vec3 dir) {
+        switch (light) {
+        case ambient:
+            type = light;
+            intensity = intens;
+            break;
+        case point:
+            type = light;
+            intensity = intens;
+            position = dir;
+            break;
+        case directional:
+            type = light;
+            intensity = intens;
+            direction = dir;
+            break;
+        }
     }
 };
 
 class Scene {
   public:
     Color background_color;
-    std::vector<Sphere> entities;
+    std::vector<Light> lights;
+    std::vector<Sphere> spheres;
 
     Scene() {}
 };
 
-vec3 canvas_to_viewport(double x, double y) {
+Color MultiplyColor(double intensity, Color color) {
+    unsigned char r = color.r * intensity > 255 ? 255 : color.r * intensity;
+    unsigned char g = color.g * intensity > 255 ? 255 : color.g * intensity;
+    unsigned char b = color.b * intensity > 255 ? 255 : color.b * intensity;
+
+    return Color{r, g, b, color.a};
+}
+
+vec3 CanvasToViewport(double x, double y) {
     return {x * viewport_width / canvas_width,
             y * viewport_height / canvas_height, distance_from_camera};
 }
@@ -65,12 +108,55 @@ std::array<double, 2> IntersectRaySphere(vec3 origin, vec3 direction,
     return t;
 }
 
-Color trace_ray(vec3 origin, vec3 direction, double t_min, double t_max,
-                Scene scene) {
+float ComputeLighting(vec3 position, vec3 normal, vec3 toward_camera,
+                      int specular, Scene scene) {
+    float intensity{0.0};
+
+    for (Light light : scene.lights) {
+        if (light.type == LightType::ambient) {
+            intensity += light.intensity;
+        } else {
+            vec3 light_vector{vec3(0.0, 0.0, 0.0)};
+            float light_intensity{0.0};
+            if (light.type == LightType::point) {
+                light_vector = light.position - position;
+                light_intensity = light.intensity;
+            } else {
+                light_vector = light.direction;
+                light_intensity = light.intensity;
+            }
+            auto normal_light_dot{dot(normal, light_vector)};
+
+            // Diffuse light
+            if (normal_light_dot > 0.0) {
+                intensity += light_intensity * normal_light_dot /
+                             (normal.length() * light_vector.length());
+            }
+
+            // Specular light
+            if (specular != -1) {
+                auto reflection{2 * normal * dot(normal, light_vector) -
+                                light_vector};
+                auto reflection_dot_to_camera{dot(reflection, toward_camera)};
+                if (reflection_dot_to_camera > 0) {
+                    intensity +=
+                        light.intensity * std::pow(reflection_dot_to_camera /
+                                                       (reflection.length() *
+                                                        toward_camera.length()),
+                                                   specular);
+                }
+            }
+        }
+    }
+    return intensity;
+}
+
+Color TraceRay(vec3 origin, vec3 direction, double t_min, double t_max,
+               Scene scene) {
     auto closest_t{std::numeric_limits<double>::infinity()};
     std::optional<Sphere> closest_shpere{};
 
-    for (auto entity : scene.entities) {
+    for (Sphere entity : scene.spheres) {
         auto t{(IntersectRaySphere(origin, direction, entity))};
         if (t_min < t[0] && t[0] < t_max && t[0] < closest_t) {
             closest_t = t[0];
@@ -85,33 +171,44 @@ Color trace_ray(vec3 origin, vec3 direction, double t_min, double t_max,
     if (closest_shpere == std::nullopt) {
         return scene.background_color;
     } else {
-        return closest_shpere->color;
+        auto position{origin + (direction * closest_t)};
+        auto normal{position - closest_shpere->center};
+        normal = normal / normal.length();
+
+        return MultiplyColor(ComputeLighting(position, normal, -direction,
+                                             closest_shpere->specular, scene),
+                             closest_shpere->color);
     }
 }
 
-Scene create_scene(Color background, std::vector<Sphere> entites) {
+Scene CreateScene(Color background, std::vector<Sphere> spheres,
+                  std::vector<Light> lights) {
     Scene scene{};
     scene.background_color = background;
-    scene.entities = entites;
+    scene.spheres = spheres;
+    scene.lights = lights;
     return scene;
 }
 
-void put_pixel(int x, int y, Color color) {
+void PutPixel(int x, int y, Color color) {
     auto screen_width{canvas_width / 2 + x};
     auto screen_height{canvas_height / 2 - y - 1};
 
     DrawPixel(screen_width, screen_height, color);
 }
-int main() {
-    const auto red{Color{255, 0, 0, 255}};
-    const auto blue{Color{0, 255, 0, 255}};
-    const auto green{Color{0, 255, 0, 255}};
-    const std::vector<Sphere> spheres{
-        Sphere(vec3(0, -1, 3), 1, Color{255, 0, 0, 255}),
-        Sphere(vec3(2, 0, 4), 1, Color{0, 0, 255, 255}),
-        Sphere(vec3(-2, 0, 4), 1, Color{0, 255, 0, 255})};
 
-    Scene scene{create_scene(RAYWHITE, spheres)};
+int main() {
+    const std::vector<Sphere> spheres{
+        Sphere(vec3(0, -1, 3), 1, Color{255, 0, 0, 255}, 500),
+        Sphere(vec3(2, 0, 4), 1, Color{0, 0, 255, 255}, 500),
+        Sphere(vec3(-2, 0, 4), 1, Color{0, 255, 0, 255}, 10),
+        Sphere(vec3(0, -5001, 0), 5000, Color{255, 255, 0, 255}, 1000)};
+    const std::vector<Light> lights{
+        Light(LightType::ambient, 0.2, vec3(0, 0, 0)),
+        Light(LightType::point, 0.6, vec3(2, 1, 0)),
+        Light(LightType::directional, 0.2, vec3(1, 4, 4))};
+
+    Scene scene{CreateScene(RAYWHITE, spheres, lights)};
 
     InitWindow(canvas_width, canvas_height, "Ray tracer");
     SetTargetFPS(60);
@@ -122,11 +219,11 @@ int main() {
 
         for (auto x{-canvas_width / 2}; x <= canvas_width / 2; x++) {
             for (auto y{-canvas_height / 2}; y <= canvas_height / 2; y++) {
-                auto d{canvas_to_viewport(x, y)};
-                auto color{trace_ray(camera_position, d, 1,
-                                     std::numeric_limits<double>::infinity(),
-                                     scene)};
-                put_pixel(x, y, color);
+                auto d{CanvasToViewport(x, y)};
+                auto color{TraceRay(camera_position, d, 1,
+                                    std::numeric_limits<double>::infinity(),
+                                    scene)};
+                PutPixel(x, y, color);
             }
         }
         EndDrawing();
--- raytracer/vec3.h
+++ raytracer/vec3.h
@@ -1,8 +1,8 @@
 #ifndef VEC3_H
 #define VEC3_H
 
-#include <iostream>
-
+#include <cmath>
+#include <ostream>
 class vec3 {
   public:
     double e[3];
@@ -17,14 +17,6 @@ class vec3 {
     vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
     double operator[](int i) const { return e[i]; }
     double &operator[](int i) { return e[i]; }
-
-    bool operator==(const vec3 &v) {
-        if (this->x() == v.x() & this->y() == v.y() && this->z() == v.z()) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 
     vec3 &operator+=(const vec3 &v) {
         e[0] += v.e[0];
@@ -41,7 +33,15 @@ class vec3 {
     }
 
     vec3 &operator/=(double t) { return *this *= 1 / t; }
+
+    double length() const { return std::sqrt(length_squared()); }
+
+    double length_squared() const {
+        return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+    }
 };
+
+using point3 = vec3;
 
 inline std::ostream &operator<<(std::ostream &out, const vec3 &v) {
     return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
@@ -70,5 +70,13 @@ inline vec3 operator/(const vec3 &v, double t) { return (1 / t) * v; }
 inline double dot(const vec3 &u, const vec3 &v) {
     return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
 }
+
+inline vec3 cross(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+inline vec3 unit_vector(const vec3 &v) { return v / v.length(); }
 
 #endif
